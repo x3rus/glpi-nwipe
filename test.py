c77cc29f59319da66b880b5bdd17301d315572b7
Thomas Boutry <thomas.boutry@x3rus.com>
2014-12-09 08:13:20 -0500

    ajout du premier test de communication avec GLPI

---
+++ test.py
@@ -0,0 +1,21 @@
+#/usr/bin/python2
+
+from glpi_client.XMLRPCClient import XMLRPCClient
+
+glpi = XMLRPCClient('http://goban.x3rus.com/glpi')
+glpi.connect("adm-glpi", "4dmPkPas4dmin")
+
+
+
+ordi=glpi.getObject(
+        itemtype='Computer',
+        id=1,
+)
+
+
+print ordi
+
+print "========================"
+toto=glpi.list_objects(itemtype='Computer')
+
+print toto
